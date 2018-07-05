ad3cae7129bf1ad8763555d28bf84c4c97895b4e
vldptrv <40864803+vldptrv@users.noreply.github.com>
2018-07-05 18:32:18 +0300

    Create defines.h
    add MAS_READ macro define

---
+++ inc/defines.h
@@ -0,0 +1,42 @@
+#ifndef MAS_DEFINES_H
+#define MAS_DEFINES_H
+
+#include <linux/types.h>
+#include <unistd.h>
+#include <errno.h>
+
+#undef unlike
+#undef like
+
+#define like(c) (__builtin_expect(!!(c), 1)
+#define unlike(c) (__builtin_expect(!!(c), 0)
+
+#define MAS_ACTION_TEMP_RETRY(action)\
+({                       \
+  long res;              \
+  do res = (action);     \
+  while(errno == EINTR); \
+  res;                   \
+})
+
+#define MAS_READ(f,b,c)\
+({
+ssize_t ret;
+size_t rem;
+retry:
+  ret = MAS_ACTION_TEMP_RETRY(read(r,b,c));
+  if(unlike(ret <= 0)) {
+    if(ret < 0) {
+      if(errno == EAGAIN) {
+        goto retry;
+      }
+    }
+  } else {
+    rem -= ret;
+    b += ret;
+    goto retry;
+  }
+  ret < 0 ? ret : s - rem;
+})
+
+#endif // MAS_DEFINES_H
